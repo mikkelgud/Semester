1e65135a7da1e10c613967e822f9afed38d465b9
Mikkel Gudmundsen <Mikkel@dhcp-10-253-0-174.wlan.hioa.no>
2019-04-05 16:58:54 +0200

    added files

---
+++ ForsikringsInfo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progutvsemester;
+
+/**
+ *
+ * @author alexanderbjorlo
+ */
+public class ForsikringsInfo extends nameContainer {
+    
+    public ForsikringsInfo(String firstName, String lastName) {
+        
+        super(firstName, lastName);
+    }
+    
+}
